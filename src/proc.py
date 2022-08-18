128fb6499c85cfdc9fd62e6c708bdf15f56142fa
Chritch <chritch-7@outlook.com>
2022-08-18 16:21:05 +0800

    [FEATURE] Remove the tag in the line.

--- src/proc.py
+++ src/proc.py
@@ -1,12 +1,19 @@
 import re
 import os
 
+def RemoveTag(line):
+    line = re.sub(r'\#{1}[^#\s]+', "", line)
+    return line
+
 def ConvertFile(file):
     # 1. convert the admonition format
+    # 2. remove the tag in the line
     with open(file, 'r') as f, open('.temp.md', 'w') as buffer:
         
         match_admonition = False
         for line in f:
+
+            # begin of the admonition
             if re.search('```ad-info', line):
                 match_admonition = True
                 buffer.write('!!! info' + '\n')
@@ -17,6 +24,10 @@ def ConvertFile(file):
                 buffer.write('\n')
                 continue
             
+            line = RemoveTag(line)
+
+            # If in admonition, add tab before the line
+            # Otherwise, output straightforwardly
             if match_admonition:
                 buffer.write('\t' + line)
             else:
